b59b4310fa79f2819f26ee32019c4be7299c00ee
Kyle Koder <silvertallon@gmail.com>
2019-06-26 21:18:45 -0700

    1.12.2.000b

--- src/main/java/com/fantasticsource/omnipotence/Omnipotence.java
+++ src/main/java/com/fantasticsource/omnipotence/Omnipotence.java
@@ -13,7 +13,7 @@ public class Omnipotence
 {
     public static final String MODID = "omnipotence";
     public static final String NAME = "Omnipotence";
-    public static final String VERSION = "1.12.2.000a";
+    public static final String VERSION = "1.12.2.000b";
 
     static
     {
