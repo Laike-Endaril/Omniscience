e83221beac907c4ab2aa6b41c13e86dcad3b00fe
Kyle Koder <silvertallon@gmail.com>
2019-07-21 14:11:14 -0700

    Added a few NBT debugging commands

--- src/main/java/com/fantasticsource/omnipotence/Commands.java
+++ src/main/java/com/fantasticsource/omnipotence/Commands.java
@@ -2,6 +2,9 @@ package com.fantasticsource.omnipotence;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
@@ -30,18 +33,19 @@ public class Commands extends CommandBase
     @Override
     public String getUsage(ICommandSender sender)
     {
-        return AQUA + "/omnipotence threads" + WHITE + " - " + I18n.translateToLocalFormatted(Omnipotence.MODID + ".cmd.threads.comment") + "\n" +
-                AQUA + "/omnipotence threads <id>" + WHITE + " - " + I18n.translateToLocalFormatted(Omnipotence.MODID + ".cmd.threads.comment2") + "\n" +
-                AQUA + "/omnipotence threads <id> stop" + WHITE + " - " + I18n.translateToLocalFormatted(Omnipotence.MODID + ".cmd.threads.comment3");
+        return AQUA + "/omnipotence threads" + WHITE + " - " + I18n.translateToLocalFormatted(Omnipotence.MODID + ".cmd.threads.comment")
+                + "\n" + AQUA + "/omnipotence threads <id>" + WHITE + " - " + I18n.translateToLocalFormatted(Omnipotence.MODID + ".cmd.threads.comment2")
+                + "\n" + AQUA + "/omnipotence threads <id> stop" + WHITE + " - " + I18n.translateToLocalFormatted(Omnipotence.MODID + ".cmd.threads.comment3")
+
+                + "\n" + AQUA + "/omnipotence nbt hand" + WHITE + " - " + I18n.translateToLocalFormatted(Omnipotence.MODID + ".cmd.nbt.comment")
+                + "\n" + AQUA + "/omnipotence nbt self" + WHITE + " - " + I18n.translateToLocalFormatted(Omnipotence.MODID + ".cmd.nbt.comment2")
+                + "\n" + AQUA + "/omnipotence nbt nearestentity" + WHITE + " - " + I18n.translateToLocalFormatted(Omnipotence.MODID + ".cmd.nbt.comment3");
     }
 
     public void execute(MinecraftServer server, ICommandSender sender, String[] args)
     {
         if (args.length == 0) sender.getCommandSenderEntity().sendMessage(new TextComponentString(getUsage(sender)));
-        else
-        {
-            subCommand(sender, args);
-        }
+        else subCommand(sender, args);
     }
 
     public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos)
@@ -49,31 +53,44 @@ public class Commands extends CommandBase
         ArrayList<String> result = new ArrayList<>();
 
         String partial = args[args.length - 1];
-        if (args.length == 1)
+        switch (args.length)
         {
-            result.add("threads");
+            case 1:
+                result.add("threads");
+                result.add("nbt");
 
-            if (partial.length() != 0) result.removeIf(k -> partial.length() > k.length() || !k.substring(0, partial.length()).equalsIgnoreCase(partial));
-        }
-        else if (args.length == 2)
-        {
-            switch (args[0])
-            {
-                case "threads":
-                    for (long id : Debug.threadIDs()) result.add("" + id);
+                if (partial.length() != 0) result.removeIf(k -> partial.length() > k.length() || !k.substring(0, partial.length()).equalsIgnoreCase(partial));
+                break;
 
-                    if (partial.length() != 0) result.removeIf(k -> partial.length() > k.length() || !k.substring(0, partial.length()).equalsIgnoreCase(partial));
-            }
-        }
-        else if (args.length == 3)
-        {
-            switch (args[0])
-            {
-                case "threads":
-                    result.add("stop");
+            case 2:
+                switch (args[0])
+                {
+                    case "threads":
+                        for (long id : Debug.threadIDs()) result.add("" + id);
 
-                    if (partial.length() != 0) result.removeIf(k -> partial.length() > k.length() || !k.substring(0, partial.length()).equalsIgnoreCase(partial));
-            }
+                        if (partial.length() != 0) result.removeIf(k -> partial.length() > k.length() || !k.substring(0, partial.length()).equalsIgnoreCase(partial));
+                        break;
+
+                    case "nbt":
+                        result.add("hand");
+                        result.add("self");
+                        result.add("nearestentity");
+
+                        if (partial.length() != 0) result.removeIf(k -> partial.length() > k.length() || !k.substring(0, partial.length()).equalsIgnoreCase(partial));
+                        break;
+                }
+                break;
+
+            case 3:
+                switch (args[0])
+                {
+                    case "threads":
+                        result.add("stop");
+
+                        if (partial.length() != 0) result.removeIf(k -> partial.length() > k.length() || !k.substring(0, partial.length()).equalsIgnoreCase(partial));
+                        break;
+                }
+                break;
         }
         return result;
     }
@@ -149,8 +166,96 @@ public class Commands extends CommandBase
                 else notifyCommandListener(sender, this, getUsage(sender));
                 break;
 
+            case "nbt":
+                switch (args.length)
+                {
+                    case 1:
+                        notifyCommandListener(sender, this, "hand");
+                        notifyCommandListener(sender, this, "self");
+                        notifyCommandListener(sender, this, "nearestentity");
+                        break;
+
+                    case 2:
+                        if (!(sender instanceof EntityPlayerMP))
+                        {
+                            notifyCommandListener(sender, this, Omnipotence.MODID + ".cmd.nbt.notPlayer");
+                            return;
+                        }
+
+                        switch (args[1])
+                        {
+                            case "hand":
+                                notifyCommandListener(sender, this, "");
+                                notifyCommandListener(sender, this, ((EntityPlayerMP) sender).getHeldItemMainhand().getDisplayName());
+                                notifyNBT(sender, ((EntityPlayerMP) sender).getHeldItemMainhand().writeToNBT(new NBTTagCompound()));
+                                notifyCommandListener(sender, this, "");
+                                break;
+
+                            case "self":
+                                notifyCommandListener(sender, this, "");
+                                notifyCommandListener(sender, this, sender.getName());
+                                notifyNBT(sender, ((EntityPlayerMP) sender).writeToNBT(new NBTTagCompound()));
+                                notifyCommandListener(sender, this, "");
+                                break;
+
+                            case "nearestentity":
+                                EntityPlayerMP player = (EntityPlayerMP) sender;
+                                Entity entity = player.world.findNearestEntityWithinAABB(Entity.class, player.getEntityBoundingBox().grow(100), player);
+                                if (entity != null)
+                                {
+                                    notifyCommandListener(sender, this, "");
+                                    notifyCommandListener(sender, this, entity.getName() + " @ " + entity.posX + ", " + entity.posY + ", " + entity.posZ);
+                                    notifyNBT(sender, entity.writeToNBT(new NBTTagCompound()));
+                                    notifyCommandListener(sender, this, "");
+                                }
+                                else notifyCommandListener(sender, this, Omnipotence.MODID + ".cmd.nbt.noEntityFound");
+                                break;
+
+                            default:
+                        }
+                        break;
+                }
+                break;
+
             default:
                 notifyCommandListener(sender, this, getUsage(sender));
+        }
+    }
+
+    private void notifyNBT(ICommandSender sender, NBTTagCompound compound)
+    {
+        char[] chars = compound.toString().toCharArray();
+        String current = "";
+        String indent = "";
+        for (int i = 0; i < chars.length; i++)
+        {
+            char c = chars[i];
+            switch (c)
+            {
+                case '{':
+                case '[':
+                    if (!current.equals("")) notifyCommandListener(sender, this, indent + current);
+                    notifyCommandListener(sender, this, indent + c);
+                    current = "";
+                    indent += " ";
+                    break;
+
+                case '}':
+                case ']':
+                    if (!current.equals("")) notifyCommandListener(sender, this, indent + current);
+                    indent = indent.substring(0, indent.length() - 1);
+                    notifyCommandListener(sender, this, indent + c + (i + 1 < chars.length && chars[i + 1] == ',' ? ',' : ""));
+                    current = "";
+                    break;
+
+                case ',':
+                    if (!current.equals("")) notifyCommandListener(sender, this, indent + current + c);
+                    current = "";
+                    break;
+
+                default:
+                    current += c;
+            }
         }
     }
 }
