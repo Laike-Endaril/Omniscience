bf6f1a775eda4cc0a97aea08ea0bee7166d18723
Kyle Koder <silvertallon@gmail.com>
2019-07-24 22:34:10 -0700

    Indev build 1.12.2.000e

--- src/main/java/com/fantasticsource/omnipotence/Omnipotence.java
+++ src/main/java/com/fantasticsource/omnipotence/Omnipotence.java
@@ -13,7 +13,7 @@ public class Omnipotence
 {
     public static final String MODID = "omnipotence";
     public static final String NAME = "Omnipotence";
-    public static final String VERSION = "1.12.2.000d";
+    public static final String VERSION = "1.12.2.000e";
 
     static
     {
