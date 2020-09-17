efe36206f9db05c4f682dfaf9d84d8f319c30996
Kyle Koder <silvertallon@gmail.com>
2020-09-16 18:57:47 -0700

    1.12.2.000l; project rename finished

--- src/main/java/com/fantasticsource/omniscience/Omniscience.java
+++ src/main/java/com/fantasticsource/omniscience/Omniscience.java
@@ -32,7 +32,7 @@ public class Omniscience
 
     public static final String MODID = "omniscience";
     public static final String NAME = "Omniscience";
-    public static final String VERSION = "1.12.2.000k";
+    public static final String VERSION = "1.12.2.000l";
 
     static
     {
