c31e8e4c8cbc9c71557521d6e128f7e71e39a0fd
Kyle Koder <silvertallon@gmail.com>
2020-11-07 22:20:56 -0800

    1.12.2.001

--- src/main/java/com/fantasticsource/omniscience/Omniscience.java
+++ src/main/java/com/fantasticsource/omniscience/Omniscience.java
@@ -32,7 +32,7 @@ public class Omniscience
 
     public static final String MODID = "omnisciencedebugger";
     public static final String NAME = "Omniscience Debugger";
-    public static final String VERSION = "1.12.2.000q";
+    public static final String VERSION = "1.12.2.001";
 
     static
     {
